d8a65b4c508d2b6c64dc1f0557bfa9f5750666e9
VHEB <vheb0316@gmail.com>
2025-03-25 11:26:57 -0300

    fix: correção do erro de chamada do css na base.html.

--- biomap/settings.py
+++ biomap/settings.py
@@ -91,6 +91,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = '/perfil/'
 
